0033619369a29df1b8422ffffae8512ab2317a5b
Igor Ferrani <igor.ferrani@gmail.com>
2020-02-10 21:43:53 -0300

    Projeto finalizado

---
+++ app/src/main/java/br/com/codelab/archcomposample/MainActivity.kt
@@ -0,0 +1,39 @@
+package br.com.codelab.archcomposample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val viewModel: ViewModel by lazy {
+        ViewModelProviders.of(this@MainActivity).get(ViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        clickButtonAction()
+
+        viewModelObserver()
+
+    }
+
+    private fun clickButtonAction() {
+        btnA.setOnClickListener { viewModel.updateTeamA() }
+        btnB.setOnClickListener { viewModel.updateTeamB() }
+        reset.setOnClickListener { viewModel.reset() }
+    }
+
+    private fun viewModelObserver(){
+        viewModel.ScoreTeamA.observe(this, Observer { value ->
+            TeamA.text = value.toString()
+        })
+        viewModel.ScoreTeamB.observe(this, Observer { value ->
+            TeamB.text = value.toString()
+        })
+    }
+}
---
+++ app/src/main/java/br/com/codelab/archcomposample/ViewModel.kt
@@ -0,0 +1,29 @@
+package br.com.codelab.archcomposample
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ViewModel : ViewModel() {
+    var ScoreTeamA = MutableLiveData<Int>().apply { value = 0 }
+    var ScoreTeamB = MutableLiveData<Int>().apply { value = 0 }
+
+    fun updateTeamA() {
+        update(ScoreTeamA)
+    }
+
+    fun updateTeamB() {
+        update(ScoreTeamB)
+
+    }
+
+    fun update(score: MutableLiveData<Int>) {
+        val value = score.value ?: 0
+        score.value = value + 1
+    }
+
+    fun reset() {
+        ScoreTeamA.value = 0
+        ScoreTeamB.value = 0
+
+    }
+}
